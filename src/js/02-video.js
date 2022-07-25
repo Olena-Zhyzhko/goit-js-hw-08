51ce114bfe141d1abe6ac7645c0fdd2e77c5708d
Olena-Zhyzhko <asil198609@gmail.com>
2022-07-26 02:07:59 +0300

    task2 - done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+import Player from '@vimeo/player';
+import { throttle } from 'lodash';
+
+const iframe = document.querySelector('#vimeo-player');
+const iframePlayer = new Player(iframe);
+
+updauteTime();
+
+iframePlayer.on('timeupdate', throttle(saveTime, 1000));
+
+
+function saveTime(currentTime) {
+    const { seconds, duration } = currentTime;
+    if (seconds === duration) {
+        localStorage.setItem("videoplayer-current-time", "0")
+        iframePlayer.off('timeupdate');
+        console.log("видео Закончилось");
+        return;
+    }
+    console.log(seconds);
+    localStorage.setItem("videoplayer-current-time", seconds);
+};
+
+
+function updauteTime() {
+    iframePlayer.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
+}
+
+// console.log(iframePlayer);
 
